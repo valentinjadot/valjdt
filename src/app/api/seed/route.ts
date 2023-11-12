cf10bf074f0ab72a6faff212fbca92fb3753e5da
Valentin <valentin@fintual.com>
2023-11-12 17:08:44 -0600

    freat(seed): add seeding API

---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+import seed from "./seed";
+
+export const runtime = "nodejs";
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+    const information = formData.get("information") as string;
+
+    seed(information);
+
+    return NextResponse.json({ success: true });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.error();
+  }
+}
