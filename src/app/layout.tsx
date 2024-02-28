33cdc5a312a9f2bf4073ca99c212bafb7d98b9df
Valentin <valentin@fintual.com>
2024-02-28 17:29:19 -0600

    feat(SEO): migrate to new NextJS SEO system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,41 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"], preload: true });
 
 export const metadata: Metadata = {
   title: "Valentin Jadot",
-  description: "creative coder",
+  description: "Valentin Jadot - creative coder",
+  metadataBase: new URL("https://valentinjadot.com"),
+  creator: "Valentin Jadot",
+  authors: [{ url: "https://valentinjadot.com", name: "Valentin Jadot" }],
+  keywords: [
+    "Valentin Jadot",
+    "creative coder",
+    "software developer",
+    "personal website",
+    "portfolio",
+    "branding",
+    "web development",
+  ],
+  viewport: "width=device-width, initial-scale=1.0",
+  robots: "index, follow",
+  themeColor: "#000000",
+  openGraph: {
+    type: "website",
+    url: "https://valentinjadot.com",
+    title: "Valentin Jadot",
+    description: "creative coder",
+    images: [
+      {
+        url: "https://valentinjadot.com/og-image.png",
+        width: 1190,
+        height: 1000,
+        alt: "Valentin Jadot",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -17,16 +45,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Valentin Jadot - creative coder" />
-        <meta property="og:title" content="Valentin Jadot" />
-        <meta property="og:description" content="creative coder" />
-        <meta property="og:type" content="website" />
-
-        <title>Valentin Jadot</title>
-      </Head>
       <body
         className={inter.className}
         style={{ width: "100vw", height: "100vh", margin: 0 }}
