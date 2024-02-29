739885542b27eb85d7c392749925ed886bce813b
Valentin <valentin@fintual.com>
2024-02-29 13:09:32 -0600

    fix(responsiveness): fix bug on smartphones with keyboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
       </head>
       <body
         className={inter.className}
-        style={{ width: "100vw", height: "100vh", margin: 0 }}
+        style={{ width: "100%", height: "100%", margin: 0 }}
       >
         <main>
           <CurrentUserProvider>{children}</CurrentUserProvider>
