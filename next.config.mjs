5306b1f68ca51201beedbefe4efaeebb1f91c61f
Valentin <valentin@fintual.com>
2024-03-11 12:57:14 -0600

    chore(redirect): add boom platform

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 import "./src/app/env.mjs";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/plataforma-boom",
+        destination:
+          "https://valentinjadot.notion.site/Plataforma-Boom-introducci-n-8b007091ff1d49bbb13734130fabfcac?pvs=4",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
