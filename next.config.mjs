117ffc3bf5ad6c896fc3987c3f2256118875b282
Valentin <valentin@fintual.com>
2024-03-13 18:20:00 -0600

    feat(redirection): mapa boom notion

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
           "https://valentinjadot.notion.site/Plataforma-Boom-introducci-n-8b007091ff1d49bbb13734130fabfcac?pvs=4",
         permanent: true,
       },
+      {
+        source: "/mapa-boom",
+        destination:
+          "https://valentinjadot.notion.site/Mapa-introducci-n-c9396c40ce994795982e63afd8b46636?pvs=4",
+        permanent: true,
+      },
     ];
   },
 };
