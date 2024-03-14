14e60841115a4faed208baf866c399bcd3cad545
Valentin <valentin@fintual.com>
2024-03-14 06:53:36 -0600

    feat(cookie): keep fingerprint in cookie

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,15 +3,20 @@ import { NextResponse } from "next/server";
 import prisma from "@/utils/prisma"; // Adjust this path to your Prisma client instance
 import { User } from "@prisma/client";
 import { geolocation, ipAddress } from "@vercel/edge";
+import { cookies } from "next/headers";
 
 export const runtime = "edge";
 
 export async function POST(request: Request) {
   try {
-    const { fingerprint } = await request.json();
+    const cookieStore = cookies();
+
+    const fingerprint =
+      getFinterprintFromCookie() ?? (await getFinterprintFromRequest(request));
+
+    cookieStore.set("fingerprint", fingerprint);
 
     const entropy = JSON.stringify({
-      request,
       ip: ipAddress(request),
       geolocation: geolocation(request),
     });
@@ -36,3 +41,12 @@ export async function POST(request: Request) {
     });
   }
 }
+
+const getFinterprintFromRequest = async (request: Request): Promise<string> => {
+  const { fingerprint } = await request.json();
+  return fingerprint;
+};
+const getFinterprintFromCookie = () => {
+  const cookieStore = cookies();
+  return cookieStore.get("fingerprint")?.value;
+};
