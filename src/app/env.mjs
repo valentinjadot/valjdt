8a5b30b5fb5fba8cc35d5e2c65de9db44b21f1b6
Valentin <valentin@fintual.com>
2024-04-15 16:16:33 -0400

    fix(model): fix env name again

--- src/app/env.mjs
+++ src/app/env.mjs
@@ -25,7 +25,7 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    OPENAI_MODEL: process.env.OPEN_AI_MODEL,
+    OPENAI_MODEL: process.env.OPENAI_MODEL,
     PINECONE_API_KEY: process.env.PINECONE_API_KEY,
     PINECONE_ENVIRONMENT: process.env.PINECONE_ENVIRONMENT,
     PINECONE_INDEX_NAME: process.env.PINECONE_INDEX_NAME,
